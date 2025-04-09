c6fcd9eed897fd85806627cf3bfe5878b9a78a8b
Damsithcoder <113232763+Damsithcoder@users.noreply.github.com>
2025-04-09 19:42:52 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ import { useEffect } from 'react'
 function App() {
   const [cards, setCards] = useState([])
   const getData=()=>{
-    fetch('./data.json')
+    fetch('https://damsithcoder.github.io/API/data.json')
       .then(function(response){
         return response.json();
       })
